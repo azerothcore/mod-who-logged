d3efa42fbd9158f645cdc8bc1dd5d59f2020dd2c
St0ny <42348051+Raz0r1337@users.noreply.github.com>
2025-05-29 13:50:40 +0200

    Update who_logged.h
    Added hook for player logout

--- src/who_logged.h
+++ src/who_logged.h
@@ -10,10 +10,12 @@ class WhoLoggedAnnounce : public PlayerScript
 {
 public:
     WhoLoggedAnnounce() : PlayerScript("WhoLoggedAnnounce", {
-        PLAYERHOOK_ON_LOGIN
+        PLAYERHOOK_ON_LOGIN,
+        PLAYERHOOK_ON_LOGOUT
     }) {}
 
     void OnPlayerLogin(Player* player) override;
+    void OnPlayerLogout(Player* player) override;
 };
 
 void AddWhoLoggedScripts()
